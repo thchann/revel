52486600f2166df242c139d7cc523756acc9e313
gustavo <gbelaund@trinity.edu>
2025-04-28 20:53:23 -0500

    root nav fixed

--- navigators/RootStackNavigator.tsx
+++ navigators/RootStackNavigator.tsx
@@ -3,20 +3,19 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { ActivityIndicator, View } from 'react-native';
 import { useAuth } from '../auth/auth-context';
 import TabsNavigator from './TabsNavigator';
-import LoginScreen from '../app/new_user_components/login'; // don't forget to import
+import LoginScreen from '../app/new_user_components/login';
 import { RootStackParamList } from '../types/navigation';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function RootStackNavigator() {
-  const { token, authLoading, userInitialized } = useAuth();
+  const { token, authLoading } = useAuth();
   const isAuthenticated = !!token;
 
   console.log('🧭 [RootNav] token:', token);
   console.log('🕓 [RootNav] authLoading:', authLoading);
-  console.log('✅ [RootNav] userInitialized:', userInitialized);
 
-  if (authLoading || (isAuthenticated && !userInitialized)) {
+  if (authLoading) {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <ActivityIndicator size="large" />
